c595412b788e01aa004823766b6b85d00311b507
LinkunGao <gaolinkun123@gmail.com>
2023-01-29 14:09:43 +1300

    add a compelet rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,8 @@
 var babel = require("@rollup/plugin-babel");
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import { uglify } from "rollup-plugin-uglify";
+require("dotenv").config({ path: "./.env" });
 
 export default {
   input: "./src/main.js",
@@ -10,14 +14,27 @@ export default {
       format: "umd",
     },
     {
-      file: "dist/bundle.esm.js",
+      file: "dist/bundle.esm.mjs",
       format: "es",
     },
   ],
+  sourceMap: "inline",
   plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
     babel({
       babelHelpers: "bundled",
       presets: [["@babel/preset-env"]],
+      exclude: "node_modules/**",
     }),
+    process.env.NODE_ENV === "production" && uglify(),
   ],
 };
+
+// 在打包时，若存在引用第三方包， 则必须要使用rollup-plugin-node-resolve和rollup-plugin-commonjs来解决，第三方的引入。
+// presets: [["@babel/preset-env",{ modules: false }]]
+// rollup-plugin-uglify 用于美化压缩代码
