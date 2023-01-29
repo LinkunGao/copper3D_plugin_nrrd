57e93dc6dad366ba6ab1327eca4e0518dd460af1
LinkunGao <gaolinkun123@gmail.com>
2023-01-29 14:19:55 +1300

    .

--- rollup.config.js
+++ rollup.config.js
@@ -28,7 +28,7 @@ export default {
     commonjs(),
     babel({
       babelHelpers: "bundled",
-      presets: [["@babel/preset-env"]],
+      presets: [["@babel/preset-env", { modules: false }]],
       exclude: "node_modules/**",
     }),
     process.env.NODE_ENV === "production" && uglify(),
@@ -38,3 +38,31 @@ export default {
 // 在打包时，若存在引用第三方包， 则必须要使用rollup-plugin-node-resolve和rollup-plugin-commonjs来解决，第三方的引入。
 // presets: [["@babel/preset-env",{ modules: false }]]
 // rollup-plugin-uglify 用于美化压缩代码
+
+// v2
+// var babel = require("@rollup/plugin-babel");
+
+// export default {
+//   input: "./src/main.js",
+
+//   output: [
+//     {
+//       name: "copper3D_nrrd_plugin",
+//       file: "dist/bundle.js",
+//       format: "umd",
+//     },
+//     {
+//       file: "dist/bundle.esm.mjs",
+//       format: "es",
+//     },
+//   ],
+//   sourceMap: "inline",
+//   plugins: [
+
+//     babel({
+//       babelHelpers: "bundled",
+//       presets: [["@babel/preset-env",{ modules: false }]],
+//     }),
+
+//   ],
+// };
